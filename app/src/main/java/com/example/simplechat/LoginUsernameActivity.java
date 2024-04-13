f97f9e94b62dab97d815ef5f782ab16cb0a4df32
andrew1205lin <andrew1205lin@gmail.com>
2024-04-13 10:25:58 +0800

    done login

--- app/src/main/java/com/example/simplechat/LoginUsernameActivity.java
+++ app/src/main/java/com/example/simplechat/LoginUsernameActivity.java
@@ -1,14 +1,33 @@
 package com.example.simplechat;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.simplechat.model.UserModel;
+import com.example.simplechat.utils.FirebaseUtil;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.Timestamp;
+import com.google.firebase.firestore.DocumentSnapshot;
+
 public class LoginUsernameActivity extends AppCompatActivity {
+    EditText usernameInput;
+    Button startChatBtn;
+    ProgressBar progressBar;
+
+    String phoneNumber;
+    UserModel userModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +39,69 @@ public class LoginUsernameActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        usernameInput = findViewById(R.id.login_username);
+        startChatBtn = findViewById(R.id.login_start_chat_btn);
+        progressBar =findViewById(R.id.login_progress_bar);
+
+        // if the user have their name in the database, they should use it
+        // otherwise, they can input their username
+
+        phoneNumber = getIntent().getExtras().getString("phone");
+        getUsername();
+        startChatBtn.setOnClickListener((v -> {
+            setUsername();
+        }));
+    }
+    void setUsername(){
+        String username = usernameInput.getText().toString();
+        if(username.isEmpty() || username.length()<3){
+            usernameInput.setError("Username length should be at least 3 chars");
+            return;
+        }
+        setInProgress(true);
+        if(userModel!=null){
+            userModel.setUsername(username);
+        }else{
+            userModel = new UserModel(phoneNumber,username, Timestamp.now());
+        }
+
+        FirebaseUtil.currentUserDetails().set(userModel).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                setInProgress(false);
+                if(task.isSuccessful()){
+                    Intent intent = new Intent(LoginUsernameActivity.this,MainActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK );// clear everything in login activity
+                    startActivity(intent);
+                }
+            }
+        });
+
+    }
+    void getUsername(){
+        setInProgress(true);
+        FirebaseUtil.currentUserDetails().get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                setInProgress(false);
+                if(task.isSuccessful()){
+                    userModel = task.getResult().toObject(UserModel.class);
+                    if(userModel!=null){
+                        usernameInput.setText(userModel.getUsername());
+                    }
+                }
+            }
+        });
+    }
+
+    void setInProgress(boolean inProgress){
+        if(inProgress){
+            progressBar.setVisibility(View.VISIBLE);
+            startChatBtn.setVisibility(View.GONE);
+        }else{
+            progressBar.setVisibility(View.GONE);
+            startChatBtn.setVisibility(View.VISIBLE);
+        }
     }
 }
