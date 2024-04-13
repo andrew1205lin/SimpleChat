d51a4fbc89a4c2f57aad2e496bf0408b69b41c01
andrew1205lin <andrew1205lin@gmail.com>
2024-04-13 18:03:11 +0800

    finish the basic layout in the main activity; finish search user adapter.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,4 +46,5 @@ dependencies {
 
     implementation(platform("com.google.firebase:firebase-bom:32.8.0"))
     implementation("com.google.firebase:firebase-auth")
+    implementation("com.firebaseui:firebase-ui-firestore:8.0.2")
 }
